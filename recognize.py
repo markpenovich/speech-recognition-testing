30669aabf01cf9626b5097a070198871422da849
Mark Penovich <Mark@Marks-MacBook-Pro.local>
2021-01-11 15:56:14 -0600

    first commit of speech to text recognition for language learning

---
+++ recognize.py
@@ -0,0 +1,58 @@
+import speech_recognition as sr
+import pyaudio
+import wave
+import unidecode
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 7
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+r = sr. Recognizer()
+
+input_audio = sr.AudioFile('output.wav')
+
+with input_audio as source:
+    audio = r.record(source)
+
+input_text_parsed = r.recognize_google(audio, language="es-US") # Read WAV file and convert to text
+input_text_cleansed = unidecode.unidecode(input_text_parsed) # Normalize text. (Convert text with accents to regular letters)
+
+print("Parsed: ",input_text_parsed)
+print("Cleansed (no accent): ", input_text_cleansed)
+
+if (input_text_cleansed.lower() == "el perro esta corriendo en la calle"):
+    print("Congrats! You got the correct word!")
+else:
+    print("Sorry, that was the wrong answer. You are STUPID!")
